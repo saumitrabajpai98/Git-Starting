a2e8f41c1e3968e10a3207aba1560e9ac1362ecd
Saumitra <bajpaisaumitra12@gmail.com>
2021-05-13 14:35:21 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+const crntDate=new Date().toLocaleDateString();
+const crntTime=new Date().toLocaleTimeString();
+let img1="https://picsum.photos/200/300";
+let img2="https://picsum.photos/250/300";
+let img3="https://picsum.photos/300/300";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<>
+<h1 contentEditable="true" class='heading'>Hola! Konnichiva!! </h1>
+<div className="imagss">
+<img src={img1} alt="Some Random Imagez" />
+<img src={img2} alt="Some Random Images" />
+<img src={img3} alt="Some Random Imagess" />
+</div>
+<div className="datetym">
+<p>{`Today's Date is: ${crntDate}`}</p>
+<p>{`Cuurent time is: ${crntTime}`}</p>
+</div>
+</>,document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
